4ee44fc440a5278afa45a3dc88087ed87c3df24e
Jochen Pfeiffer <git@jochen-pfeiffer.com>
2017-09-25 21:33:23 +0200

    sudo keap alive

--- setup.sh
+++ setup.sh
@@ -5,7 +5,14 @@ fancy_echo() {
 	printf "\n$fmt\n" "$@"
 }
 
+
 set -e
+
+# Ask for password up-front
+sudo -v
+
+# Keep-alive: update existing sudo time stamp if set, otherwise do nothing.
+while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
 
 
 # install homebrew
